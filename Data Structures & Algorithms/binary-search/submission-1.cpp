434237b40b6fa6c8c66a07f132cc5ba7401c5b40
davidchen20 <110264004+davidchen20@users.noreply.github.com>
2026-07-13 21:37:20 -0400

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return helper(nums, 0, nums.size(), target);
+    }
+
+    int helper(vector<int>& nums, int left, int right, int target) {
+        if (left >= right) return -1;
+
+        int mid = (left + right) / 2;
+        if (nums[mid] < target) return helper(nums, mid + 1, right, target);
+        else if (nums[mid] > target) return helper(nums, left, mid, target);
+        else return mid;
+    }
+};
