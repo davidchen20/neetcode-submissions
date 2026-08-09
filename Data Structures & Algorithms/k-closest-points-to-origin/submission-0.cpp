af66947a3473c4697122ba29e57843aae506fd88
davidchen20 <110264004+davidchen20@users.noreply.github.com>
2026-08-09 13:24:14 -0400

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    struct Comparator {
+        float distance(vector<int>& p1) {
+            int xDiff = p1[0] - 0;
+            int yDiff = p1[1] - 0;
+
+            return sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+        bool operator()(vector<int>& p1, vector<int>& p2) {
+            return distance(p1) > distance(p2);
+        }
+    };
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, Comparator> pq(points.begin(), points.end());
+
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
