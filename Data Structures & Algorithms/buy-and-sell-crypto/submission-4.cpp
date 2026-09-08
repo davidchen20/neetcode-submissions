518b489c661adda655992c396c3b2b8a7ee1a0cf
davidchen20 <110264004+davidchen20@users.noreply.github.com>
2026-09-08 15:29:35 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0;
+
+        int maxProfit = 0;
+
+        for (int right = 0; right < prices.size(); right++) {
+            if (prices[right] < prices[left]) {
+                left = right;
+            }
+
+            maxProfit = max(maxProfit, prices[right] - prices[left]);
+        }
+
+        return maxProfit;
+    }
+};
