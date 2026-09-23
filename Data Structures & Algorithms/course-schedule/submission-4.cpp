d45734a0acb4b33cf716c74905c5e13c0167d3df
davidchen20 <110264004+davidchen20@users.noreply.github.com>
2026-09-22 20:20:50 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // detect any cycles
+        unordered_map<int, vector<int>> preMap;
+        unordered_set<int> visited;
+
+        for (auto& prereq : prerequisites) {
+            preMap[prereq[0]].push_back(prereq[1]);
+        }
+
+        // check each course for a cycle
+        for (int i = 0; i < numCourses; i++) {
+            
+            if (!dfs(preMap, visited, i)) return false;
+        }
+
+        return true;
+    }
+
+    bool dfs(unordered_map<int, vector<int>>& preMap, unordered_set<int>& visited, int c) {
+        if (visited.count(c)) return false;
+        if (preMap[c].empty()) return true;
+        visited.insert(c);
+        vector<int>& prereqs = preMap[c];
+        for (auto prereq : prereqs) {
+            if (!dfs(preMap, visited, prereq)) return false;
+        }
+        visited.erase(c);
+        preMap[c].clear();
+
+        return true;
+    }
+};
