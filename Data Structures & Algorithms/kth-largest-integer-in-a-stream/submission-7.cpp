866a9d39a5fcbf83275bceb2b785ac6927e955c2
davidchen20 <110264004+davidchen20@users.noreply.github.com>
2026-09-24 15:09:32 -0400

    Add: kth-largest-integer-in-a-stream - submission-7

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-7.cpp
@@ -0,0 +1,20 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+    KthLargest(int k, vector<int>& nums) : k(k), pq(nums.begin(), nums.end()) {
+        // for (int i = 0; i < nums.size(); i++) {
+        //     pq.push(nums[i]);
+        //     if (pq.size() > k) pq.pop();
+        // }
+        while (pq.size() > k) pq.pop();
+    }
+    
+    int add(int val) {  
+        pq.push(val);
+
+        if (pq.size() > k) pq.pop();
+
+        return pq.top();
+    }
+};
