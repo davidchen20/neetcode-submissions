694f28034f34101079aba79a7c4f6504ab6d2877
davidchen20 <110264004+davidchen20@users.noreply.github.com>
2026-09-24 18:39:42 -0400

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> counts;
+        for (auto task : tasks) counts[task]++;
+
+        priority_queue<int, vector<int>, less<int>> pq;
+
+        for (auto& count : counts) {
+            pq.push(count.second);
+        }
+
+        queue<pair<int, int>> availability;
+
+        int time = 0;
+        while (!pq.empty() || !availability.empty()) {
+            if (!availability.empty()) {
+                if (availability.front().second == time) {
+                    pq.push(availability.front().first);
+                    availability.pop();
+                }
+            }
+
+            if (!pq.empty()) {
+                int lettersLeft = pq.top();
+                pq.pop();
+
+                if (lettersLeft - 1 > 0) availability.push({lettersLeft - 1, time + n + 1});
+            }
+
+            if (pq.empty() && !availability.empty()) time = availability.front().second;
+            else time++;
+        }
+
+        return time;
+    }
+};
